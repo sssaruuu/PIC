3fd6494a7b47f3e98620180ef602049ec8c9b47d
sssaruuu <sssarahp07@gmail.com>
2026-01-15 13:11:06 +0530

    Data types and variables in C

---
+++ p2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    char ch = 'A';
+    int num = 10;
+    float pi = 3.14;
+    double d = 10110.123435;
+
+    printf("The value of ch is %c\n", ch);
+    printf("The value of ch is %d\n", num);
+    printf("The value of ch is %.2f\n", pi);
+    printf("The value of ch is %.5lf\n", d);
+}
